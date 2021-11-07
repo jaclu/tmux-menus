279d4479c6e9a55a1c0bdf7aefa0012d9d9a3561
jaclu <jacob.lundqvist@gmail.com>
2021-11-07 17:09:33 +0100

    removed some debug prints

--- scripts/kill_current_session.sh
+++ scripts/kill_current_session.sh
@@ -17,26 +17,21 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 force_directive="force"
 
-echo "$(date) param 1 [$1]" >> /tmp/session_kill.log
 
 if [ "$1" != "$force_directive" ]; then
-    echo "$(date) not called with force" >> /tmp/session_kill.log
     ses_count="$(tmux list-sessions | wc -l)"
     if [ "$ses_count" -eq 1 ]; then
 	$CURRENT_DIR/kill_session_confirm.sh
 	exit 0
-	echo" $(date) ERROR: continued after confirm exit!" >> /tmp/session_kill.log
     fi
 fi
 
 ses_to_go="$(tmux display-message -p '#S')"
 
 if [ -z "$ses_to_go" ]; then
-   echo "$(date) ses_to_go empty" >> /tmp/session_kill.log
    exit 0
 fi
 
-echo "$(date) ses_to_go [$ses_to_go]" >> /tmp/session_kill.log
 
 
 #  switch to random next session
