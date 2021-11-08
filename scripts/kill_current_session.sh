7f23321e1ea129a1864d2aeedba74024d564b733
jaclu <jacob.lundqvist@gmail.com>
2021-11-08 19:58:05 +0100

    added explaination of the sleep statement

--- scripts/kill_current_session.sh
+++ scripts/kill_current_session.sh
@@ -15,12 +15,21 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
 force_directive="force"
+
+ses_count="$(tmux list-sessions | wc -l)"
+
+ses_to_go="$(tmux display-message -p '#{session_id}')"
+
+if [ -z "$ses_to_go" ]; then
+    tmux display-message -d 5000 "ERROR: tmux-menus:kill_current_session.sh  Failed to identify current session!"
+    exit 0
+fi
 
 
 if [ "$1" != "$force_directive" ]; then
-    ses_count="$(tmux list-sessions | wc -l)"
-    if [ "$ses_count" -eq 1 ]; then
+    if [ $ses_count -lt 2 ]; then
 	$CURRENT_DIR/kill_session_confirm.sh
 	
 	#
@@ -31,18 +40,27 @@ if [ "$1" != "$force_directive" ]; then
     fi
 fi
 
-ses_to_go="$(tmux display-message -p '#{session_id}')"
-
-if [ -z "$ses_to_go" ]; then
-   exit 0
-fi
-
-
 
 #  switch to random next session
 tmux switch-client -n &
 
-sleep 2
+
+if [ $ses_count -gt 1 ]; then
+    #
+    #  On some slower systems, like iSH it might actually take a second to
+    #  switch sessions. If the client is still connected to the session when
+    #  it gets killed it gets disconnected. Nothing major, the other sessions
+    #  are still running, but its an unnesesarry anoyannce to have to
+    #  reconnect. Combined with that this is not blocking anything,
+    #  running it its own thread, it doesnt hurt to wait a bit.
+    #
+    #  If it is the last session, this slowness doesnt matter, it would
+    #  switch back to itself anyhow, and regardless if the kill happens before
+    #  or after the switch the end result is the same.
+    #
+    sleep 2
+fi
+
 
 tmux kill-session -t "$ses_to_go"
  
