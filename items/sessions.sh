c383969d7219b68119a7d6593a93a9e29eb245b1
jaclu <jacob.lundqvist@gmail.com>
2021-11-08 19:55:30 +0100

    some code cleanup

--- items/sessions.sh
+++ items/sessions.sh
@@ -22,7 +22,8 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-script_dir="$(dirname "$CURRENT_DIR")/scripts"
+SCRIPT_DIR="$(dirname "$CURRENT_DIR")/scripts"
+
 
 tmux display-menu  \
      -T "#[align=centre] Sessions "  \
@@ -30,9 +31,9 @@ tmux display-menu  \
      \
      "Back to main-menu"  Left  "run-shell $CURRENT_DIR/main.sh"  \
      "" \
-     "<P> Rename this session"         $   "command-prompt -I \"#S\" \"rename-session -- '%%'\""  \
-     "    New session"                 n  "command-prompt -p \"Name of new session: \" \"new-session -s '%%'\""  \
-     "    Kill current session"        k "confirm-before -p \"Are you sure you want to kill this session ? (y/n)\" \"run \"$script_dir/kill_current_session.sh\"\" "  \
+     "<P> Rename this session"   $  "command-prompt -I \"#S\" \"rename-session -- '%%'\""  \
+     "    New session"           n  "command-prompt -p \"Name of new session: \" \"new-session -s '%%'\""  \
+     "    Kill current session"  k  "confirm-before -p \"Are you sure you want to kill this session ? (y/n)\" \"run \"${SCRIPT_DIR}/kill_current_session.sh\"\" "  \
      "" \
      "    Choose session, use arrows" ""  ""  \
      "<P>         to navigate & zoom"   s   "choose-tree -Zs"  \
