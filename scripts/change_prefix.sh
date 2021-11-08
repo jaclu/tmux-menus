c5fa07d2e18c33c20e511c218137e5701a7fff2f
jaclu <jacob.lundqvist@gmail.com>
2021-11-08 15:34:19 +0100

    support scriptt for advanced-Cange prefix

---
+++ scripts/change_prefix.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+#   Copyright (c) 2021: Jacob.Lundqvist@gmail.com
+#   License: MIT
+#
+#   Part of https://github.com/jaclu/tmux-menus
+#
+#   Version: 1.0 2021-11-08
+#       Initial release
+#
+#   Updates global prefix, if prefix param is given
+#
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+prefix_char="$1"
+
+if [ -z "$prefix_char" ]; then
+    tmux display-message -d 5000 "ERROR: tmux-menus: No prefix given!"
+    exit 0
+fi
+
+prefix="C-$prefix_char"
+
+tmux set-option -g prefix "$prefix"
+
+tmux display-message -d 5000 "Be aware <prefix> is now: $prefix"
