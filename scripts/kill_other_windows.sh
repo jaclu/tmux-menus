d41356011b96c1b15ad471f0e7a585d73bc0de69
jaclu <jacob.lundqvist@gmail.com>
2021-11-14 17:43:15 +0100

    Added K - kill all other windows

---
+++ scripts/kill_other_windows.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+#   Copyright (c) 2021: Jacob.Lundqvist@gmail.com
+#   License: MIT
+#
+#   Part of https://github.com/jaclu/tmux-menus
+#
+#   Version: 1.0 2021-11-14
+#       Initial release
+#
+#   Kill all other windows
+#
+
+window_list="$(IFS=" " tmux list-windows -F '#{window_id}')"
+current_window="$(tmux display-message -p '#{window_id}')"
+
+for w in $window_list; do
+	if [ "$w" != "$current_window" ]; then
+		tmux kill-window -t "$w"
+	fi
+done
