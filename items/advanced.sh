e9b9626e5a65de842d8577877ba756fabb6b7f86
jaclu <jacob.lundqvist@gmail.com>
2021-12-03 01:41:31 +0100

    added ses/win/pane navigation

--- items/advanced.sh
+++ items/advanced.sh
@@ -5,7 +5,7 @@
 #
 #   Part of https://github.com/jaclu/tmux-menus
 #
-#   Version: 1.1 2021-11-11
+#   Version: 1.1.1 2021-12-03
 #
 #   This is the advanced menu, wit more archaic tasks
 #
@@ -44,12 +44,15 @@ tmux display-menu \
      \
      "Back to main-menu"  Left  "run-shell $CURRENT_DIR/main.sh"  \
      "" \
-     "    Toggle mouse to: $new_mouse_status"   "m"  "set-option -g mouse $new_mouse_status"  \
-     "    Change prefix <$current_prefix>"  p  "command-prompt -1 -p prefix 'run \"$SCRIPT_DIR/change_prefix.sh %%\"'"  \
+     "    Navigate & zoom to ses/win/pane" ""  ""  \
+     "<P>   use arrows to navigate & zoom"   s   "choose-tree -Zs"  \
      "" \
      "<P> Show messages"        \~     show-messages        \
      "<P> Customize options"     C     "customize-mode -Z"  \
      "<P> Describe key binding"  /     "command-prompt -k -p key \"list-keys -1N \\"%%%\\"\""  \
      "<P> Prompt for a command"  :     command-prompt  \
+     "" \
+     "    Toggle mouse to: $new_mouse_status"   "m"  "set-option -g mouse $new_mouse_status"  \
+     "    Change prefix <$current_prefix>"  p  "command-prompt -1 -p prefix 'run \"$SCRIPT_DIR/change_prefix.sh %%\"'"  \
      "" \
      "Help"  h  "run-shell \"$CURRENT_DIR/help.sh $CURRENT_DIR/advanced.sh\""
