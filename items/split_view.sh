90e6479d8bb8f9b4d3db30e5ac11ac61681414e3
Jaclu <Jacob.Lundqvist@gmail.com>
2022-01-22 01:06:34 +0000

    Increased width of Labeled separator lines, tidied up alignment of menu code, updated syntax description

--- items/split_view.sh
+++ items/split_view.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 #
-#   Copyright (c) 2021: Jacob.Lundqvist@gmail.com
+#   Copyright (c) 2021,2022: Jacob.Lundqvist@gmail.com
 #   License: MIT
 #
 #   Part of https://github.com/jaclu/tmux-menus
 #
-#   Version: 1.0.1 2021-12-21
+#   Version: 1.0.2 2022-01-22
 #
-#   menu dealing with panes
+#   Menu dealing with splitting panes/windows.
 #
-#   There are three types of menu item lines:
+#   Types of menu item lines.
+#
 #   1) An item leading to an action
-#       "Description" "in menu shortcut key" " action taken when it is triggered"
-#       For any field containing no spaces quotes are optional
+#          "Description" "In-menu key" "Action taken when it is triggered"
+#
 #   2) Just a line of text
-#       "Some text to display" "" ""
+#      You must supply two empty strings, in order for the
+#      menu logic to interpret it as a full menu line item.
+#          "Some text to display" "" ""
+#
 #   3) Separator line
-#       ""
+#      This is a propper gaphical separator line, without any label.
+#          ""
+#
+#   4) Labeled separator line
+#      Not pefect, since you will have at least one space on each side of
+#      the labeled separator line, but using something like this and carefully
+#      increase the dashes until you are just below forcing the menu to just
+#      grow wider, seems to be as close as it gets.
+#          "#[align=centre]-----  Other stuff  -----" "" ""
+#
+#
 #   All but the last line in the menu, needs to end with a continuation \
-#   Whitespace after thhis \ will cause the menu to fail!
+#   Whitespace after this \ will cause the menu to fail!
+#   For any field containing no spaces, quotes are optional.
 #
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -32,16 +47,16 @@ tmux display-menu  \
      -T "#[align=centre] Split view "        \
      -x $menu_location_x -y $menu_location_y \
      \
-     "Back to main-menu"       Left  "run-shell $CURRENT_DIR/main.sh"  \
-     "#[align=centre]---  Split Pane  ---" "" ""             \
-     "Left"  l "split-window -hb  -c '#{pane_current_path}'" \
-     "Right" r "split-window -h -c '#{pane_current_path}'"   \
-     "Above" a "split-window -vb -c '#{pane_current_path}'"  \
-     "Below" b "split-window -v  -c '#{pane_current_path}'"  \
-     "#[align=centre]--  Split Window  --" "" ""             \
-     "Left"  L "split-window -fhb -c '#{pane_current_path}'" \
-     "Right" R "split-window -fh  -c '#{pane_current_path}'" \
-     "Above" A "split-window -fvb -c '#{pane_current_path}'" \
-     "Below" B "split-window -fv  -c '#{pane_current_path}'" \
+     "Back to main-menu"  Left  "run-shell $CURRENT_DIR/main.sh"  \
+     "#[align=centre]-----  Split Pane  -----" "" ""              \
+     "Left"   l  "split-window -hb   -c '#{pane_current_path}'"   \
+     "Right"  r  "split-window -h    -c '#{pane_current_path}'"   \
+     "Above"  a  "split-window -vb   -c '#{pane_current_path}'"   \
+     "Below"  b  "split-window -v    -c '#{pane_current_path}'"   \
+     "#[align=centre]----  Split Window  ----" "" ""              \
+     "Left"   L  "split-window -fhb  -c '#{pane_current_path}'"   \
+     "Right"  R  "split-window -fh   -c '#{pane_current_path}'"   \
+     "Above"  A  "split-window -fvb  -c '#{pane_current_path}'"   \
+     "Below"  B  "split-window -fv   -c '#{pane_current_path}'"   \
      "" \
-     "Help"  h  "run-shell \"$CURRENT_DIR/help_split.sh $CURRENT_DIR/split_view.sh\""
+     "Help"   h  "run-shell \"$CURRENT_DIR/help_split.sh $CURRENT_DIR/split_view.sh\""
