92476ab8f5ae19b4f12c0fc25b492802a86ed90e
jaclu <jacob.lundqvist@gmail.com>
2022-04-03 04:22:34 +0200

    activated log_file

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -25,7 +25,7 @@ plugin_name="tmux-menus"
 #  If log_file is empty or undefined, no logging will occur,
 #  so comment it out for normal usage.
 #
-# log_file="/tmp/$plugin_name.log"  # Trigger LF to separate runs of this script
+log_file="/tmp/$plugin_name.log"  # Trigger LF to separate runs of this script
 
 
 #
