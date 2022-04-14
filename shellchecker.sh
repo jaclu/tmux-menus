ae03701b3868aecdae2df6760c8d29317b839eae
jaclu <jacob.lundqvist@gmail.com>
2022-04-14 02:37:47 +0200

    made it more informative

--- shellchecker.sh
+++ shellchecker.sh
@@ -1,19 +1,36 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 #   Copyright (c) 2022: Jacob.Lundqvist@gmail.com
 #   License: MIT
 #
 #   Part of https://github.com/jaclu/tmux-menus
 #
-#   Version: 1.0.0 2022-04-14
+#   Version: 1.0.1 2022-04-14
 #
 #   Does shellcheck on all relevant scripts in this project
 #
 
-# shellcheck disable=SC1007
+#  shellcheck disable=SC1007
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-shellcheck "$CURRENT_DIR"/menus.tmux
-shellcheck "$CURRENT_DIR"/shellchecker.sh
-shellcheck "$CURRENT_DIR"/scripts/*.sh
-shellcheck "$CURRENT_DIR"/items/*.sh
+cd "$CURRENT_DIR" || return
+
+
+checkables=(
+
+    #  Obviousl self exam should be done :)
+    shellchecker.sh
+
+    menus.tmux
+
+    scripts/*.sh
+
+    items/*.sh
+)
+
+for script in "${checkables[@]}"; do
+    # abort as soon as one gives warnings
+    echo "Cecking: $script"
+    shellcheck -x "$script" || exit 1
+
+done
