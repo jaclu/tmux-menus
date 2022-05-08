35073cf9974c17b03d3c2d82ec1f09c7002efb66
jaclu <jacob.lundqvist@gmail.com>
2022-05-08 17:33:08 +0200

    streamlined code

--- items/window_move.sh
+++ items/window_move.sh
@@ -5,38 +5,13 @@
 #
 #   Part of https://github.com/jaclu/tmux-menus
 #
-#   Version: 1.3.0 2022-05-06
+#   Version: 1.3.1  2022-05-08
 #
 #   Move Window
 #
-#   Main menu, the one popping up when you hit the trigger
-#
-#   Types of menu item lines.
-#
-#   1) An item leading to an action
-#          "Description" "In-menu key" "Action taken when it is triggered"
-#
-#   2) Just a line of text
-#      You must supply two empty strings, in order for the
-#      menu logic to interpret it as a full menu line item.
-#          "Some text to display" "" ""
-#
-#   3) Separator line
-#      This is a proper graphical separator line, without any label.
-#          ""
-#
-#   4) Labeled separator line
-#      Not perfect, since you will have at least one space on each side of
-#      the labeled separator line, but using something like this and carefully
-#      increase the dashes until you are just below forcing the menu to just
-#      grow wider, seems to be as close as it gets.
-#          "#[align=centre]-----  Other stuff  -----" "" ""
-#
-#
-#   All but the last line in the menu, needs to end with a continuation \
-#   White space after this \ will cause the menu to fail!
-#   For any field containing no spaces, quotes are optional.
-#
+
+#  shellcheck disable=SC2034
+#  Directives for shellcheck directly after bang path are global
 
 # shellcheck disable=SC1007
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
@@ -49,6 +24,11 @@ menu_name="Move Window"
 req_win_width=41
 req_win_height=15
 
+this_menu="$CURRENT_DIR/window_move.sh"
+reload="; run-shell \"$this_menu\""
+to_other="choose-tree -Gw 'run-shell \"$SCRIPT_DIR/relocate_window.sh W M %%\"'"
+link_other="choose-tree -Gw 'run-shell \"$SCRIPT_DIR/relocate_window.sh W L %%\"'"
+
 
 t_start="$(date +'%s')"
 
@@ -57,19 +37,20 @@ tmux display-menu  \
      -T "#[align=centre] $menu_name "  \
      -x "$menu_location_x" -y "$menu_location_y"  \
      \
-     "Back to Main menu"        Home  "run-shell $CURRENT_DIR/main.sh"  \
-     "Back to Handling Window"  Left  "run-shell $CURRENT_DIR/windows.sh" \
+     "Back to Main menu"        Home  "run-shell $CURRENT_DIR/main.sh"      \
+     "Back to Handling Window"  Left  "run-shell $CURRENT_DIR/windows.sh"   \
      "" \
-     "Move window to other location"    m  "choose-tree -Gw 'run-shell \"$SCRIPT_DIR/relocate_window.sh W M %%\"'" \
-     "-#{?pane_marked_set,#[nodim],}Swap current window with window" "" ""  \
-     "#{?pane_marked_set,,-} containing marked pane        "  s  swap-window  \
-     "Swap window Left"                \<  "swap-window -dt:-1 ; run-shell \"$CURRENT_DIR/window_move.sh\""  \
-     "Swap window Right"               \>  "swap-window -dt:+1 ; run-shell \"$CURRENT_DIR/window_move.sh\""  \
+     "Move window to other location"      m  "$to_other"                    \
+     "-#{?pane_marked_set,#[nodim],}Swap current window with window"  "" "" \
+     "#{?pane_marked_set,,-} containing marked pane"                        \
+                                          s  swap-window                    \
+     "Swap window Left"                  \<  "swap-window -dt:-1 $reload"   \
+     "Swap window Right"                 \>  "swap-window -dt:+1 $reload"   \
      "" \
-     "Link window to other session"     l  "choose-tree -Gw 'run-shell \"$SCRIPT_DIR/relocate_window.sh W L %%\"'" \
-     "Unlink window from this session"  u  "unlink-window" \
+     "Link window to other session"       l  "$link_other"                  \
+     "Unlink window from this session"    u  "unlink-window"                \
      "" \
-     "Help, explaining move & link  -->"  H  "run-shell \"$CURRENT_DIR/help_window_move.sh\""
+     "Help, explaining move & link  -->"  H  "run-shell \"$this_menu\""
 
 
 ensure_menu_fits_on_screen
