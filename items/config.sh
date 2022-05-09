ed3d94c5fa47248c6d46324efe515d28777b3feb
Jacob Lundqvist <jaclu@users.noreply.github.com>
2022-05-09 20:23:16 +0200

    Streamlined menu

--- items/config.sh
+++ items/config.sh
@@ -5,7 +5,7 @@
 #
 #   Part of https://github.com/jaclu/tmux-menus
 #
-#   Version: 0.0.1 2022-05-09
+#   Version: 0.0.2 2022-05-09
 #
 #   Main menu, the one popping up when you hit the trigger
 #
@@ -29,27 +29,33 @@ open_menu="run-shell $CURRENT_DIR"
 this_menu="$CURRENT_DIR/config.sh"
 reload="; $this_menu"
 move_menu="run-shell '$SCRIPT_DIR/move_menu.sh $req_win_width $req_win_height"
+
+set_coords="command-prompt \
+    -I \"$cached_location_x\",\"$cached_location_y\" \
+    -p \"horizontal param (max: #{window_width}) \",\"vertical param (max: #{window_height}) \
+    \" \"$move_menu coord %1 %2'\""
+
+
 t_start="$(date +'%s')"  #  if the menu closed in < 1s assume it didnt fit
 
 
 
 # shellcheck disable=SC2154
 tmux display-menu \
-     -T "#[align=centre] $menu_name "             \
-     -x "$menu_location_x" -y "$menu_location_y"  \
-     \
-     "Back to Main menu"      Left  "$open_menu/main.sh"    \
-     "" \
-     "Center"           C  "$move_menu C $reload'"      \
-     "Right side"       R  "$move_menu R $reload'"      \
-     "Pane bot left"    P  "$move_menu P $reload'"      \
-     "Win pos status"   W  "$move_menu W $reload'"      \
-     "By status line"   S  "$move_menu S $reload'"      \
-     "set coordinates"  c  "command-prompt -I \"$cached_location_x\",\"$cached_location_y\"   \
-        -p \"horizontal param (max: #{window_width}) \",\"vertical param (max: #{window_height}) \" \"$move_menu coord %1 %2 $reload'\""  \
+    -T "#[align=centre] $menu_name "             \
+    -x "$menu_location_x" -y "$menu_location_y"  \
+    \
+    "Back to Main menu"  Left  "$open_menu/main.sh"  \
     "" \
-     "-When using coordinates" "" "" \
-     "-lower left corner is set!" "" ""
+    "Center"           C  "$move_menu C $reload'"    \
+    "Right side"       R  "$move_menu R $reload'"    \
+    "Pane bot left"    P  "$move_menu P $reload'"    \
+    "Win pos status"   W  "$move_menu W $reload'"    \
+    "By status line"   S  "$move_menu S $reload'"    \
+    "set coordinates"  c  "$set_coords  $reload"     \
+    "" \
+    "-When using coordinates" "" "" \
+    "-lower left corner is set!" "" ""
 
 
 
