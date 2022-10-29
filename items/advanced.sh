d0b3c96746186ef1ed2bb2c665ee641e0a7d9e17
jaclu <jacob.lundqvist@gmail.com>
2022-10-29 07:13:04 +0200

    Added left arrow for back to main

--- items/advanced.sh
+++ items/advanced.sh
@@ -42,11 +42,11 @@ describe_prefix="command-prompt -k -p key 'list-keys -1N \"%%%\"'"
 toggle_mouse="set-option -g mouse $new_mouse_status"
 kill_server="confirm-before -p 'kill tmux server on #H ? (y/n)' kill-server"
 
-set --  "command-prompt -1 -p \"prefix (will take effect imeditally)\""     \
+set --  "command-prompt -1 -p \"prefix (will take effect imeditally)\""       \
         "'run \"$SCRIPT_DIR/change_prefix.sh %%\"'"
 change_prefix="$*"
 
-set --  "#{?@menus_config_overrides,Plugin configuration"                   \
+set --  "#{?@menus_config_overrides,Plugin configuration"                     \
         " -->,-Configuration disabled}"
 plugin_conf_prompt="$*"
 
@@ -54,26 +54,26 @@ plugin_conf_prompt="$*"
 t_start="$(date +'%s')"
 
 # shellcheck disable=SC2154,SC2140
-$TMUX_BIN display-menu                                                      \
-    -T "#[align=centre] $menu_name "                                        \
-    -x "$menu_location_x" -y "$menu_location_y"                             \
-                                                                            \
-    "Back to Main menu"     Left  "$open_menu/main.sh'"                     \
-    "Manage clients  -->"  M     "$open_menu/advanced_manage_clients.sh'"   \
-    ""                                                                      \
-    "<P> List all key bindings"          \?  "list-keys -N"                 \
-    "<P> Describe (prefix) key"           /  "$describe_prefix"             \
-    "<P> Show messages"                  \~  show-messages                  \
-    "<P> Customize options"               C  "customize-mode -Z"            \
-    "<P> Prompt for a command"            :  command-prompt                 \
-    ""                                                                      \
-    "Toggle mouse to: $new_mouse_status"  m  "$toggle_mouse $reload"        \
-    "Change prefix <$current_prefix>"     p  "$change_prefix"               \
-    ""                                                                      \
-    "-#[nodim]Kill server - all your sessions"                 "" ""        \
-    " on this host are terminated    "    x  "$kill_server"                 \
-    ""                                                                      \
-    "$plugin_conf_prompt"                 P  "$open_menu/config.sh'"        \
+$TMUX_BIN display-menu                                                        \
+    -T "#[align=centre] $menu_name "                                          \
+    -x "$menu_location_x" -y "$menu_location_y"                               \
+                                                                              \
+    "Back to Main menu  <--"     Left  "$open_menu/main.sh'"                  \
+    "Manage clients     -->"  M     "$open_menu/advanced_manage_clients.sh'"  \
+    ""                                                                        \
+    "<P> List all key bindings"          \?  "list-keys -N"                   \
+    "<P> Describe (prefix) key"           /  "$describe_prefix"               \
+    "<P> Show messages"                  \~  show-messages                    \
+    "<P> Customize options"               C  "customize-mode -Z"              \
+    "<P> Prompt for a command"            :  command-prompt                   \
+    ""                                                                        \
+    "Toggle mouse to: $new_mouse_status"  m  "$toggle_mouse $reload"          \
+    "Change prefix <$current_prefix>"     p  "$change_prefix"                 \
+    ""                                                                        \
+    "-#[nodim]Kill server - all your sessions"                 "" ""          \
+    " on this host are terminated    "    x  "$kill_server"                   \
+    ""                                                                        \
+    "$plugin_conf_prompt"                 P  "$open_menu/config.sh'"          \
     "Help  -->"  H  "$open_menu/help.sh $this_menu'"
 
 ensure_menu_fits_on_screen
