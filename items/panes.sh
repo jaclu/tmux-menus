c49867791ec284904ce657cef8a065062ade68fe
jaclu <jacob.lundqvist@gmail.com>
2023-04-19 17:36:27 +0200

    updated min vers for panes

--- items/panes.sh
+++ items/panes.sh
@@ -34,10 +34,9 @@ set -- \
         'select-pane -T \"%%\"' $menu_reload" \
     1.8 C z "<P> Zoom pane toggle" "resize-pane -Z $menu_reload" \
     1.7 C q "<P> Display pane numbers" "display-panes $menu_reload" \
-    2.0 C "[" '<P> Copy mode - "history"' "copy-mode" \
+    1.8 C "[" '<P> Copy mode - "history"' "copy-mode" \
     2.1 C m "<P> $new_mark_state current pane" "select-pane -m $menu_reload" \
-    1.7 C s "    Display pane size" "display-message \
-        'Pane: #P size: #{pane_width}x#{pane_height}' $menu_reload" \
+    1.7 C s "    Display pane size" "display-message 'Pane: #P size: #{pane_width}x#{pane_height}' $menu_reload" \
     0.0 S \
     1.9 C y " $new_sync_state synchronized panes" "set -w synchronize-panes" \
     2.0 C h "Save pane history no escapes" "command-prompt -p \
