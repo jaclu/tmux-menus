0eba72361680273670a735e73eac380e47626aa1
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2023-12-13 09:54:56 +0100

    making pane-border-lines changes global

--- items/layouts.sh
+++ items/layouts.sh
@@ -15,6 +15,17 @@ SCRIPT_DIR="$(dirname "$ITEMS_DIR")/scripts"
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR/dialog_handling.sh"
 
+#
+# Odd, initially setw -g failed on my macbook, but now it seems
+# to work, probably due to pushing this change ? :)
+#
+#if [ "$(uname)" = "Linux" ]; then
+    setw_cmd="setw -g"
+#else
+#    setw_cmd="setw"
+#fi
+#echo "><> setwcmd [$setw_cmd]"
+
 menu_name="Layouts"
 
 #  shellcheck disable=SC2154
@@ -29,11 +40,11 @@ set -- \
     0.0 C E "<P> Spread evenly" "select-layout -E  $menu_reload" \
     3.2 S \
     3.2 T "-#[align=centre,nodim]Border lines" \
-    3.2 C "s" "single" "setw pane-border-lines  single  $menu_reload" \
-    3.2 C "d" "double" "setw pane-border-lines  double  $menu_reload" \
-    3.2 C "h" "heavy" "setw  pane-border-lines  heavy   $menu_reload" \
-    3.2 C "S" "simple" "setw pane-border-lines  simple  $menu_reload" \
-    3.2 C "n" "number" "setw pane-border-lines  number  $menu_reload" \
+    3.2 C "s" "single" "$setw_cmd pane-border-lines  single  $menu_reload" \
+    3.2 C "d" "double" "$setw_cmd pane-border-lines  double  $menu_reload" \
+    3.2 C "h" "heavy" "$setw_cmd pane-border-lines  heavy   $menu_reload" \
+    3.2 C "S" "simple" "$setw_cmd pane-border-lines  simple  $menu_reload" \
+    3.2 C "n" "number" "$setw_cmd pane-border-lines  number  $menu_reload" \
     0.0 S \
     0.0 M H "Help -->" "$ITEMS_DIR/help.sh $current_script"
 
