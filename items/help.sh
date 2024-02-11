4a26fa00266c675a1415ce9f449922e18b8b3837
jaclu <jacob.lundqvist@gmail.com>
2024-02-11 18:54:17 +0100

    updated help how to quit

--- items/help.sh
+++ items/help.sh
@@ -39,7 +39,7 @@ static_content() {
         0.0 S \
         0.0 T "-#[nodim]Shortcut keys are usually upper case" \
         0.0 T "-#[nodim]for menus, and lower case for actions." \
-        0.0 T "-#[nodim]Exit menus with ESC or q"
+        0.0 T "-#[nodim]Exit menus with ESC or Ctrl-C"
 
     menu_generate_part 2 "$@"
 }
