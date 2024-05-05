2337ea0d334169fdd945dfb12e9f6c11644d69c4
jaclu <jacob.lundqvist@gmail.com>
2024-05-05 14:51:03 +0200

    commented out most code for linting purposes

--- scripts/move_menu.sh
+++ scripts/move_menu.sh
@@ -8,6 +8,8 @@
 #
 #   Moving current pane within same session or to other session.
 #
+#   THIS IS NOT USED ATM!
+#
 
 #  Full path to tmux-menux plugin
 D_TM_BASE_PATH="$(realpath -- "$(dirname -- "$(dirname -- "$0")")")"
@@ -21,8 +23,8 @@ _this="move_menu.sh" # error prone if script name is changed :(
 [ "$current_script" != "$_this" ] && error_msg "$_this should NOT be sourced"
 
 action="$1"
-param_1="$2"
-param_2="$3"
+# param_1="$2"
+# param_2="$3"
 
 if [ -z "$action" ]; then
     error_msg "move_menu.sh was called without action param"
@@ -31,20 +33,20 @@ fi
 #  It will be created with defaults if not present
 read_config
 
-if [ "$action" = "C" ]; then
-    location_x="C"
-    location_y="C"
-elif [ "$action" = "R" ]; then
-    location_x="R"
-elif [ "$action" = "P" ]; then
-    location_x="P"
-    location_y="P"
-elif [ "$action" = "W" ]; then
-    location_x="W"
-    location_y="S"
-elif [ "$action" = "coord" ]; then
-    location_x="$param_1"
-    location_y="$param_2"
-fi
+# if [ "$action" = "C" ]; then
+#     location_x="C"
+#     location_y="C"
+# elif [ "$action" = "R" ]; then
+#     location_x="R"
+# elif [ "$action" = "P" ]; then
+#     location_x="P"
+#     location_y="P"
+# elif [ "$action" = "W" ]; then
+#     location_x="W"
+#     location_y="S"
+# elif [ "$action" = "coord" ]; then
+#     location_x="$param_1"
+#     location_y="$param_2"
+# fi
 
 write_config
