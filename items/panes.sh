0881c4146428574b2e1a3db2806d58b602fb37bb
jaclu <jacob.lundqvist@gmail.com>
2024-12-20 01:45:46 +0100

    made panes fully cacheable

--- items/panes.sh
+++ items/panes.sh
@@ -8,43 +8,20 @@
 #   Handling pane
 #
 
-dynamic_content() {
-    # Things that change dependent on various states
-
-    new_mark_state="$(tmux_error_handler display -p '#{?pane_marked,Unmark,Mark}')"
-    new_sync_state="$(tmux_error_handler display -p '#{?pane_synchronized,Disable,Activate}')"
-
-    # dynamic -2
-    if [ "$(tmux_error_handler display -p '#{window_zoomed_flag}')" -eq 0 ]; then
-        zoom_action="Zoom"
-    else
-        zoom_action="Un-Zoom"
-    fi
-
-    set -- \
-        1.8 C z "$zoom_action pane" "resize-pane -Z $menu_reload" \
-        2.1 C m "$new_mark_state current pane" "select-pane -m $menu_reload" \
-        1.9 C y "$new_sync_state synchronized panes" "set -w synchronize-panes"
-
-    menu_generate_part 2 "$@"
-}
-
 static_content() {
-
-    # # f_cache_file_panes="${f_cache_file}-panes"
-    # zoom_action_placeholder="===Zoom-or-UnZoom==="
-
-    # static - 1
+    if [ -d "$HOME"/tmp ]; then
+        d_history="$HOME"/tmp
+    else
+        d_history="$d_tmp"
+    fi
     set -- \
         0.0 M Left "Back to Main menu    $nav_home" main.sh \
         0.0 M M "Move pane            $nav_next" pane_move.sh \
         0.0 M R "Resize pane          $nav_next" pane_resize.sh \
-        0.0 S
-
-    menu_generate_part 1 "$@"
-
-    # static -3
-    set -- \
+        0.0 S \
+        1.8 C z "Toggle pane zoom" "resize-pane -Z $menu_reload" \
+        2.1 C m "Toggle mark current pane" "select-pane -m $menu_reload" \
+        1.9 C y "Toggle synchronized panes" "set -w synchronize-panes $menu_reload" \
         2.6 C c "Clear screen & history" \
         "send-keys C-l ; run 'sleep 0.3' ; clear-history" \
         1.8 C h 'history (enter \"copy mode\")' "copy-mode" \
@@ -55,10 +32,10 @@ static_content() {
             'Pane: #P size: #{pane_width}x#{pane_height}' $menu_reload" \
         0.0 S \
         2.0 C s "Save pane history no escapes" "command-prompt -p \
-            'Save to (no escapes):' -I '~/tmux.history' \
+            'Save to (no escapes):' -I '$d_history/tmux-history' \
             'capture-pane -S - -E - ; save-buffer %1 ; delete-buffer'" \
         2.0 C e "Save pane history with escapes" "command-prompt -p \
-            'Save to (with escapes):' -I '~/tmux.history' \
+            'Save to (with escapes):' -I '$d_history/tmux-history-escapes' \
             'capture-pane -S - -E - -e ; save-buffer %1 ; delete-buffer'" \
         0.0 S \
         2.0 C r "Respawn current pane" "confirm-before -p \
@@ -71,7 +48,8 @@ static_content() {
         0.0 S \
         0.0 M H "Help $nav_next" "$d_items/help_panes.sh $f_current_script"
 
-    menu_generate_part 3 "$@"
+    menu_generate_part 1 "$@"
+    unset d_history
 }
 
 #===============================================================
