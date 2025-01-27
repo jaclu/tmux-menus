2d73ce04505afc5b4c4fc3ef2060d95bed80f3c8
jaclu <jacob.lundqvist@gmail.com>
2025-01-27 07:36:28 +0100

    seems to work

--- scripts/update_custom_inventory.sh
+++ scripts/update_custom_inventory.sh
@@ -7,77 +7,96 @@
 #   Part of https://github.com/jaclu/tmux-menus
 #
 #  This is triggered by menus.tmux as plugin is initiated.
-#  But can also be run manually, to trigger the re-examination of items/custom/
+#  But can also be run manually, to trigger the re-examination of custom_items/
 #
-#  If any changes in the files found in items/custom/is observed, either
-#  an index menu items/custom/_index.sh is created or updated,
+#  If any changes in the files found in custom_items/ is observed, either
+#  an index menu custom_items/_index.sh is created or updated,
 #  listing each custom menu.
 #
-#  If no other files are found there, the items/custom/_index.sh is removed.
+#  If no other files are found there, the custom_items/_index.sh is removed.
 #
 #  If the _index.sh is created/removed the cache for the Main Menu is purged.
-#  Next time the Main Menu is run, it will check if items/custom/_index.sh
+#  Next time the Main Menu is run, it will check if custom_items/_index.sh
 #  is present. If it is, then a menu pointer to this menu is included.
 #
-# Menus plaed in items/custom can provide two custom hints
-# 1. menu label - this will be used in _index.sh to describe the menu.
-# 2. shortcut   - what shortcut to use for this menu in _index.sh
-#
-# If either is missing, a tmux notification will be presented mentioning the issue
-# and _index.sh will not be updated to lst the flawed custom menu.
-# Once the custom menu has been updated, the next run of this script will
-# notice that something changed and will try again to generate a _index.sh pointing
-# to all present custom menus.
 
-clear_custom_items_cache() {
-    [ -z "$d_cache" ] && error_msg "variable d_cache was unexpectedly undefined!"
+clear_cache_main() {
+    # log_it "clear_cache_main()"
+    # when:
+    #  no custom_items > custom_items
+    #  custom_items > no custom_items
     # clear cache for main menu, so that next time its run it will not include
     # the alternate items index
+
     rm -rf "$d_cache"/items_main
-    # remove all cached custom items
-    rm -rf "$d_cache"/custom_items_*/
+}
+
+clear_custom_content_template() {
+    # log_it "clear_custom_content_template()"
     # remove temp file - items being added to custom menu
     rm -f "$f_custom_items_content"
 }
 
-remove_custom_index() {
-    # make sure its gone
-    rm -f "$f_custom_items_index"
-    clear_custom_items_cache # just to be sure its not pointing this file
+clear_cache_custom_items() {
+    # log_it "clear_cache_custom_items()"
+    [ -z "$d_cache" ] && error_msg "variable d_cache was unexpectedly undefined!"
+
+    # remove all cached custom items
+    rm -rf "$d_cache"/custom_items_*/
+
+    rm -f "$f_chksum_custom"
+
+    # only time this should not be done is when cache...
+    [ "$1" != "keep_content_template" ] && clear_custom_content_template
 }
 
+remove_custom_item_content() {
+    # Remove custom item index page and all related caches
+    rm -f "$f_custom_items_index"
+    clear_cache_custom_items # just to be sure its not pointing this file
+}
 
 checksum_content_read() {
-    [ -f "$f_chksum_custom" ] && cat "$f_chksum_custom"
+    # log_it "checksum_content_read()"
+    if [ -f "$f_chksum_custom" ]; then
+        cat "$f_chksum_custom"
+    fi
 }
 
 checksum_content_write() {
-    find "$d_custom_items" -type f -exec sha256sum {} + | sort | \
-	sha256sum > "$f_chksum_custom" || {
+    # log_it "checksum_content_write()"
+    find "$d_custom_items/" -type f -exec sha256sum {} + | sort |
+        sha256sum >"$f_chksum_custom" || {
 
-	error_msg "Failed to write checksum into: $f_chksum_custom"
+        error_msg "Failed to write checksum into: $f_chksum_custom"
     }
 }
 
-content_changed_check() {
-    log_it "content_changed_check()"
+custom_items_changed_check() {
+    # log_it "custom_items_changed_check()"
 
     previous_content_chksum="$(checksum_content_read)"
-    log_it " previous: $previous_content_chksum"
+    # log_it " previous: $previous_content_chksum"
 
     checksum_content_write
     current_content_chksum="$(checksum_content_read)"
     [ -z "$current_content_chksum" ] && {
-	error_msg "Failed to scan content in: $d_custom_items"
+        error_msg "Failed to scan content in: $d_custom_items"
     }
-    log_it " current:  $current_content_chksum"
+    # log_it " current:  $current_content_chksum"
 
     [ "$previous_content_chksum" != "$current_content_chksum" ]
 }
 
 get_variable_from_script() {
+    #
+    # exposed variables:
+    #   variable_content
+    #
     _file="$1"
     _variable_to_verify="$2"
+    _show_error="$3"
+    # log_it "get_variable_from_script($_file, $_variable_to_verify, $_show_error)"
     _code_snippet="$(grep ^"${_variable_to_verify}"= "$_file")"
     [ -z "$_code_snippet" ] && {
         error_msg "$_file does not define $_variable_to_verify" -1
@@ -91,7 +110,12 @@ get_variable_from_script() {
         return 1
     }
     run_in_sub_shell="$(printf '%s\n%s' "$_code_snippet" "echo \$$_variable_to_verify")"
-    _variable_content=$(sh -c "$run_in_sub_shell")
+    variable_content=$(sh -c "$run_in_sub_shell")
+
+    [ "$_show_error" = "show_error" ] && [ -z "$variable_content" ] && {
+        error_msg "$custom_menu: $_variable_to_verify was empty"
+    }
+    return 0
 }
 
 failed_to_extract_variable() {
@@ -101,99 +125,99 @@ failed_to_extract_variable() {
 }
 
 create_custom_index() {
-    log_it "create_custom_index()"
+    # log_it "create_custom_index()"
     crate_cache_folder # make sure it exists
     [ -z "$f_custom_items_content" ] && {
         error_msg "variable f_custom_items_content undefined"
     }
     rm -f "$f_custom_items_content" # make sure its nothing there
+    clear_cache_main
 
     for custom_menu in $1; do
 
         # since the get_variable_from_script calls succeeded just before as the
         # script was validated, lets assume it works again...
-        get_variable_from_script "$custom_menu" menu_key || {
-            failed_to_extract_variable "$custom_menu" menu_key
+        _variable="menu_key"
+        get_variable_from_script "$custom_menu" "$_variable" show_error || {
+            failed_to_extract_variable "$custom_menu" "$_variable"
             continue
         }
-        [ -z "$_variable_content" ] && {
-            error_msg "menu_key was empty in: $custom_menu"
-        }
-        n_menu_key="$_variable_content"
-        get_variable_from_script "$custom_menu" menu_name || {
-            failed_to_extract_variable "$custom_menu" menu_label
+        _menu_key="$variable_content"
+
+        _variable="menu_name"
+        get_variable_from_script "$custom_menu" "$_variable" show_error || {
+            failed_to_extract_variable "$custom_menu" "$_variable"
             continue
         }
-        [ -z "$_variable_content" ] && {
-            error_msg "menu_label was empty in: $custom_menu"
-        }
-        n_menu_item="$_variable_content"
+        _menu_name="$variable_content"
+
         [ -f "$f_custom_items_content" ] && {
             # add items continuation to previous item
             printf ' \\\n' >>"$f_custom_items_content"
         }
-	# this generates an item that will be added to custom items index
+        # this generates an item that will be added to custom items index
         printf '        %s \\\n        %s' \
-            "0.0 M \"$n_menu_key\" \"$n_menu_item   $cfg_nav_next\"" \
+            "0.0 M \"$_menu_key\" \"$_menu_name   $cfg_nav_next\"" \
             "$custom_menu" >>"$f_custom_items_content"
         log_it "Will use: $custom_menu"
     done
     [ ! -f "$f_custom_items_content" ] && {
-        # all supposedly valid custom items failed to be processed, clear
-        # main-menu cache and abort generating this index
+        # All supposedly valid custom items failed to be processed,
+        # abort generating this index
+        remove_custom_item_content
         log_it "WARNING: Despite valid custom menus was found, none could be added"
-        remove_custom_index
         return 1
     }
     echo >>"$f_custom_items_content" # adding final lf
 
     # make sure cache is cleared
-    clear_custom_items_cache
+    clear_cache_custom_items keep_content_template
 
     # Generate custom index menu
     sed "/$template_splitter/q" "$f_custom_items_template" | sed '$d' \
         >"$f_custom_items_index"
     cat "$f_custom_items_content" >>"$f_custom_items_index"
+    clear_custom_content_template
+
     sed -n "/$template_splitter/"',$p' "$f_custom_items_template" | sed '1d' \
         >>"$f_custom_items_index"
     chmod 0755 "$f_custom_items_index"
     checksum_content_write # custom index change
-
     # Verify that custom_items/_index.sh was correctly generated
     run_in_sub_shell="$(printf 'export TMUX_MENUS_NO_DISPLAY=1\n%s\n' "$f_custom_items_index")"
-    _variable_content=$(sh -c "$run_in_sub_shell")
+    variable_content=$(sh -c "$run_in_sub_shell")
 }
 
 process_custom_items() {
     # This index will be regenerated
     # If it would be present during the folder scann it would be added to the list
     # of menus to be listed within it :)
-    log_it "process_custom_items()"
+    # log_it "process_custom_items()"
     rm -f "$f_custom_items_index"
 
     # create list of runnable scripts in this folder
     # the name filter is intended to filter out foo.sh~ and foo.bash~ names
-    runables="$(find "$d_custom_items/" -type f -name '*sh'  -executable)"
-
+    runables="$(find "$d_custom_items" -maxdepth 1 -type f -name '*sh' -perm -u=x)"
     # shellcheck disable=SC2068 # in this case we want to split the param
     valid_menus=""
     for custom_menu in $runables; do
-	# log_it " examining: $custom_menu"
         #
         # Verify the existence of the two variables needed to handle it
         # as an custom menu
         #
-        get_variable_from_script "$custom_menu" menu_name || continue
-        n_menu_item="$_variable_content"
         get_variable_from_script "$custom_menu" menu_key || continue
-        n_menu_key="$_variable_content"
+        # log_it "><> found: menu_key"
+
+        get_variable_from_script "$custom_menu" menu_name || continue
+        # log_it "><> found: menu_name"
 
         valid_menus="$valid_menus $custom_menu"
-        log_it "Validated src: $custom_menu"
+        # log_it "Validated src: $custom_menu"
     done
     [ -z "$valid_menus" ] && {
         # none of the custom items are valid abort generation
-        remove_custom_index
+
+        remove_custom_item_content
         log_it "No valid custom items found"
         return 1
     }
@@ -217,18 +241,27 @@ f_chksum_custom="$d_cache"/chksum_custom_content
 
 template_splitter="CUSTOM_ITEMS_SPLITTER" # items will be inserted at his point
 f_custom_items_template="$D_TM_BASE_PATH"/items/custom_index_template.sh
+# File used during generation of custom image index
+# as each custom item is verified, it is added here
+# then inserted into the custom index and removed
 f_custom_items_content="$d_cache"/custom_items_content
 
 # debug helper
-[ "$1" = "stderr" ] && log_interactive_to_stderr=true
+[ "$DBG_TO_STDERR" = "1" ] && log_interactive_to_stderr=true
 
 if [ ! -d "$d_custom_items" ]; then
-    # Folder missing, clear cache and exit
-    clear_custom_items_cache
+    # Folder missing, clear custom items cache and exit
+    [ -f "$f_chksum_custom" ] && {
+        log_it "No longer present: $d_custom_items"
+        # only clear main menu cache if custom menus is present
+        # otherwise it would be cleared on each plugin startip
+        clear_cache_main
+    }
+    remove_custom_item_content
     exit 0
 fi
 
-if content_changed_check; then
+if custom_items_changed_check; then
     process_custom_items
 else
     log_it "No changes detected in: $d_custom_items"
