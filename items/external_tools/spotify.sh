442ced827c3446f4adfadd9ad9ceb20859a0f521
jaclu <jacob.lundqvist@gmail.com>
2025-03-06 02:49:22 +0100

    improved display of running track

--- items/external_tools/spotify.sh
+++ items/external_tools/spotify.sh
@@ -8,16 +8,14 @@
 #   Directly control Spotify
 #
 
-dynamic_content() {
-    if [ "$(uname)" = "Darwin" ]; then
-        # This title check is a MacOS script
-        set -- \
-            0.0 C t "Title - now playing" "display \
-                '$("$d_scripts"/spotify-now-playing | sed "s/'/*/g" | sed 's/"/*/g')' \
-                $menu_reload" \
-            0.0 S
-        menu_generate_part 2 "$@"
-    fi
+display_currently_playing_track() {
+    . "$D_TM_BASE_PATH"/scripts/helpers.sh
+
+    track="$(spotify status track)"
+    artist="$(spotify status artist)"
+    album="$(spotify status album)"
+    tmux_error_handler display "$track - Artist: $artist - Album: $album"
+    exit 0
 }
 
 static_content() {
@@ -29,23 +27,22 @@ static_content() {
     set -- \
         0.0 M Left "Back to Extras     $nav_prev" extras.sh \
         0.0 M Home "Back to Main menu  $nav_home" main.sh \
-        0.0 S
-
-    menu_generate_part 1 "$@"
-
-    set -- \
-        0.0 E Space "Pause/Resume" "spotify pause  $reload_no_output" \
-        0.0 E n "Next" "spotify   next   $reload_no_output" \
-        0.0 E p "Prev" "spotify   prev   $reload_no_output" \
-        0.0 E r "Replay" "spotify replay $reload_no_output" \
-        0.0 E i "Copy URI to clipboard" "spotify share uri $reload_no_output" \
-        0.0 E l "Copy URL to clipboard" "spotify share url $reload_no_output" \
+        0.0 S \
+        0.0 E t "Title - currently playing track" \
+        "$f_current_script -t $reload_no_output" \
+        0.0 S \
+        0.0 E Space "Pause/Resume" "spotify pause    $reload_no_output" \
+        0.0 E n "Next" "spotify             next     $reload_no_output" \
+        0.0 E p "Prev" "spotify             prev     $reload_no_output" \
+        0.0 E r "Replay" "spotify           replay   $reload_no_output" \
         0.0 E s "Shuffle - toggle" "spotify toggle shuffle $reload_no_output" \
         0.0 E R "Repeat  - toggle" "spotify toggle repeat  $reload_no_output" \
-        0.0 E u "vol Up" "spotify           vol up         $reload_no_output" \
-        0.0 E d "vol Down" "spotify         vol down       $reload_no_output"
+        0.0 E u "vol Up" "spotify           vol up   $reload_no_output" \
+        0.0 E d "vol Down" "spotify         vol down $reload_no_output" \
+        0.0 E i "Copy URI to clipboard" "spotify share uri $reload_no_output" \
+        0.0 E l "Copy URL to clipboard" "spotify share url $reload_no_output"
 
-    menu_generate_part 3 "$@"
+    menu_generate_part 1 "$@"
 }
 
 #===============================================================
@@ -58,6 +55,11 @@ menu_name="Spotify"
 
 #  Full path to tmux-menux plugin
 D_TM_BASE_PATH="$(dirname -- "$(dirname -- "$(dirname -- "$(realpath "$0")")")")"
+
+case "$1" in
+"-t") display_currently_playing_track ;;
+*) ;;
+esac
 
 # shellcheck source=scripts/dialog_handling.sh
 . "$D_TM_BASE_PATH"/scripts/dialog_handling.sh
