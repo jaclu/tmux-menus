07802c45d771240d295d23ff041272f29da62272
jaclu <jacob.lundqvist@gmail.com>
2025-03-07 22:30:47 +0100

    logging

--- scripts/utils/cache.sh
+++ scripts/utils/cache.sh
@@ -66,7 +66,7 @@ cache_add_ok_vers() {
     #  Add param to list of good versions (<=running tmux vers),
     #  if it wasn't cached already
     #
-    # log_it "cache_add_ok_vers($1)"
+    log_it "cache_add_ok_vers($1)"
     $cfg_use_cache || return 0
     [ -z "$1" ] && error_msg_safe "cache_add_ok_vers() - no param"
 
@@ -106,13 +106,10 @@ cache_save_known_tmux_versions() { # tmux stuff
     #  The order the versions are saved doesn't matter,
     #  since they are checked with a case to speed things up
     #
-    log_it "cache_save_known_tmux_versions() - called when not using cache"
-    $cfg_use_cache || {
-        # log_it "cache_save_known_tmux_versions() - called when not using cache"
-        return 1
-    }
     log_it "cache_save_known_tmux_versions() - $0"
-
+    $cfg_use_cache || {
+        error_msg "cache_save_known_tmux_versions() - called when not using cache"
+    }
     cache_prepare
 
     #region known tmux versions
