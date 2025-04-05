080f86471545043a595cc53a751a321c64c1ee55
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2025-04-05 12:18:21 +0200

    Now examines both prefix and root tables

--- scripts/show_cmd.sh
+++ scripts/show_cmd.sh
@@ -8,46 +8,64 @@
 #   Part of https://github.com/jaclu/tmux-menus
 #
 #  Add extra line in menu displaying what command an action would use
-#  if the command is available using a prefix bind, display this instead of the cmd
+#  if the command is available using a key bind, display this instead of the cmd
 #
 
-prefix_replace_cmd() {
-    # If a single prefix cmd matches display it as a prefix bind instead of command
-    log_it "prefix_replace_cmd($cmd_bare)"
+key_table_replace_cmd() {
+    #
+    # If multiple key bindings matches, use the first.
+    # Uses and potentially modifies the external variable: cmd_bare
+    #
+    log_it "key_table_replace_cmd($cmd_bare)"
+    tbl_type="$1"
+    [ -z "$tbl_type" ] && error_msg "key_table_replace_cmd() - no param"
 
-    _prefix_cmd="$($TMUX_BIN list-keys | grep -iv mouse | grep "$cmd_bare" |
-        sed -n 's/^[^ ][^ ]*[ ][ ]*-T[ ][ ]*prefix[ ][ ]*\([^ ][^ ]*\).*/<Prefix> \1/p')"
-    [ -n "$_prefix_cmd" ] && {
-        # _lc="$(echo "$_prefix_cmd" | wc -l)"
-        _lc=$(printf '%s' "$_prefix_cmd" | awk 'END{print NR}')
-        [ "$_lc" != "1" ] && return # multiple prefix matches, display command as is
+    # Filters out any unwanted key binds
+    _cmd="$(
+        $TMUX_BIN list-keys |
+            grep "\-T $tbl_type" |                     # Only keep $tbl_type
+            grep -v display-menu |                     # filter out the default menus
+            sed "s/-T $tbl_type/|/" | cut -d'|' -f 2 | # filters out part before -T $tbl_type
+            grep "$cmd_bare\$" | head -n 1 |           # only keep first match ending in $cmd_bare
+            awk '{ print $1 }'                         # display the bind without any whitespace
+    )"
+
+    [ -n "$_cmd" ] && {
+        # Label the key binding type
+        case "$tbl_type" in
+        prefix) _cmd="<Prefix> $_cmd" ;;
+        root) _cmd="[No Prefix] $_cmd" ;;
+        *) error_msg "key_table_replace_cmd() - param must be prefix or root" ;;
+        esac
 
         #
         # In some cases the \ needs to be kept for a prefix sequence in order
         # for it to be displayed in a menu, as those are displayed the
         # menu will filter out this remaining esc char so that it is not displayed
         #
-        last_char=$(expr "$_prefix_cmd" : '.*\(.\)$')
+        last_char=$(expr "$_cmd" : '.*\(.\)$')
         log_it "><> last_char [$last_char]"
         case "$last_char" in
         ';' | '"')
-            # Some characters needs to be prefixed in order to be displayed in a menu
-            cmd_bare="$_prefix_cmd"
+            # Some characters needs to remain prefixed in order to be displayed in a menu
+            cmd_bare="$_cmd"
             log_it "><> keeping esc char: [$cmd_bare]"
             ;;
         *)
-            # Extract \ char prefix from <Prefix> listigs
-            cmd_bare="$(echo "$_prefix_cmd" | sed 's/\\//g')"
+            # Extract \ char prefix
+            cmd_bare="$(echo "$_cmd" | sed 's/\\//g')"
             ;;
         esac
+        return 0
     }
+    return 1
 }
 
 show_cmd() {
     #
     # First filter out menu_reload components if present
-    # then try to match command to a prefix key-bind. If a match is foond
-    # display the prefix sequence matching the cmd, otherwise display the command uses
+    # then try to match command to a key bind. If a match is foond
+    # display the key bind matching the cmd, otherwise display the command
     #
     _s1="${1%" $menu_reload"}"             # skip menu_reload suffix if found
     _s2="${_s1%" $reload_in_runshell"}"    # skip reload_in_runshell suffix if found
@@ -59,9 +77,10 @@ show_cmd() {
     log_it "show_cmd($cmd_bare)"
     [ -z "$cmd_bare" ] && error_msg "show_cmd() - no command could be extracted"
 
-    prefix_replace_cmd
+    # First see if there is a prefix match, if not check root table
+    key_table_replace_cmd prefix || key_table_replace_cmd root
 
-    cmd="$cmd_bare"
+    cmd="$(echo "$cmd_bare" | sed 's/#/##/g')" # prevents tmux from expanding #{} constructs
     while [ -n "$cmd" ]; do
         chunk=$(printf '%s\n' "$cmd" | awk -v max="$cfg_display_cmds_cols" '
         {
