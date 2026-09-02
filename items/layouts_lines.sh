0fd575310a92309703b25d371576224b0dfe4582
jaclu <jacob.lundqvist@gmail.com>
2026-09-02 14:21:04 +0200

    introduced 3.8 features in layouts_lines.sh and also general cleanup of it

--- items/layouts_lines.sh
+++ items/layouts_lines.sh
@@ -8,35 +8,47 @@
 #   Define how pane lines should be displayed
 #
 
-handle_pane_title() {
+handle_pane_border_status() {
     t_opt="pane-border-status"
     _cmd="set-option -w $t_opt" # only change on a per window basis
     lbl_off="Off"
     lbl_top="Top"
     lbl_bottom="Bottom"
-    win_option="$($TMUX_BIN show-options -wv "$t_opt")"
+    lbb_top_float="top-floating"
+    lbl_bot_float="bottom-floating"
 
-    if [ -n "$win_option" ]; then
-        case "$win_option" in
-            off) lbl_off="-Off" ;;
-            top) lbl_top="-Top" ;;
-            bottom) lbl_bottom="-Bottom" ;;
-            *) error_msg "Unknown $t_opt -w option: $win_option" ;;
-        esac
-    else
-        glob_option="$($TMUX_BIN show-options -gv "$t_opt")"
-        case "$glob_option" in
-            off) lbl_off="-(glob) Off" ;;
-            top) lbl_top="-(glob) Top" ;;
-            bottom) lbl_bottom="-(glob) Bottom" ;;
-            *) error_msg "Unknown $t_opt -g option: $glob_option" ;;
-        esac
-    fi
-
+    win_option=$($TMUX_BIN show-options -wv "$t_opt")
+    glob_option=$($TMUX_BIN show-options -gv "$t_opt")
+    [ -z "$win_option" ] && [ -z "$glob_option" ] && {
+        # only fall-back to global if there is any
+        win_option=single
+    }
+    case "$win_option" in
+        off) lbl_off="-$lbl_off" ;;
+        top) lbl_top="-$lbl_top" ;;
+        bottom) lbl_bottom="-$lbl_bottom" ;;
+        top-floating) lbb_top_float="-$lbb_top_float" ;;
+        bottom-floating) lbl_bot_float="-$lbl_bot_float" ;;
+        "") # No win option - use global as current
+            case "$glob_option" in
+                off) lbl_off="-(global) $lbl_off" ;;
+                top) lbl_top="-(global) $lbl_top" ;;
+                bottom) lbl_bottom="-(global) $lbl_bottom" ;;
+                top-floating) lbb_top_float="-(global) $lbb_top_float" ;;
+                bottom-floating) lbl_bot_float="-(global) $lbl_bot_float" ;;
+                *) error_msg "Unknown global option: $t_opt [$glob_option]" ;;
+            esac
+            ;;
+        *) error_msg "Unknown win option: $t_opt [$win_option]" ;;
+    esac
     set -- \
-        2.5 C o "$lbl_off" "$_cmd     off     $runshell_reload_mnu" \
-        2.5 C t "$lbl_top" "$_cmd     top     $runshell_reload_mnu" \
-        2.5 C b "$lbl_bottom" "$_cmd  bottom  $runshell_reload_mnu"
+        0.0 T "-" \
+        0.0 T "-#[align=centre,nodim]pane-border-status" \
+        2.3 C o "$lbl_off" "$_cmd  off              $runshell_reload_mnu" \
+        2.3 C t "$lbl_top" "$_cmd  top              $runshell_reload_mnu" \
+        2.3 C b "$lbl_bottom" "$_cmd  bottom           $runshell_reload_mnu" \
+        3.7z C 1 "$lbb_top_float" "$_cmd  top-floating     $runshell_reload_mnu" \
+        3.7z C 2 "$lbl_bot_float" "$_cmd  bottom-floating  $runshell_reload_mnu"
     menu_generate_part 4 "$@"
 }
 
@@ -46,46 +58,58 @@ handle_pane_border_lines() {
     lbl_single="Single"
     lbl_double="Double"
     lbl_heavy="Heavy"
-    lbl_simple="sImple"
+    lbl_simple="Simple"
     lbl_number="Number"
     lbl_spaces="Spaces"
+    lbl_none="None"
+
     win_option="$($TMUX_BIN show-options -wv "$t_opt")"
+    glob_option="$($TMUX_BIN show-options -gv "$t_opt")"
+    [ -z "$win_option" ] && [ -z "$glob_option" ] && {
+        # only fall-back to global if there is any
+        win_option=single
+    }
+    case "$win_option" in
+        single) lbl_single="-$lbl_single" ;;
+        double) lbl_double="-$lbl_double" ;;
+        heavy) lbl_heavy="-$lbl_heavy" ;;
+        simple) lbl_simple="-$lbl_simple" ;;
+        number) lbl_number="-$lbl_number" ;;
+        spaces) lbl_spaces="-$lbl_spaces" ;;
+        none) lbl_none="-$lbl_none" ;;
+        "") # No win option - use global as current
+            case "$glob_option" in
+                single) lbl_single="-(global) $lbl_single" ;;
+                double) lbl_double="-(global) $lbl_double" ;;
+                heavy) lbl_heavy="-(global) $lbl_heavy" ;;
+                simple) lbl_simple="-(global) $lbl_simple" ;;
+                number) lbl_number="-(global) $lbl_number" ;;
+                spaces) lbl_spaces="-(global) $lbl_spaces" ;;
+                none) lbl_none="-(global) $lbl_none" ;;
+                *) error_msg "Unknown global option: $t_opt [$glob_option]" ;;
+            esac
+            ;;
+        *) error_msg "Unknown win option: $t_opt [$win_option]" ;;
+    esac
 
-    if [ -n "$win_option" ]; then
-        case "$win_option" in
-            single) lbl_single="-Single" ;;
-            double) lbl_double="-Double" ;;
-            heavy) lbl_heavy="-Heavy" ;;
-            simple) lbl_simple="-sImple" ;;
-            number) lbl_number="-Number" ;;
-            spaces) lbl_spaces="-Spaces" ;;
-            *) error_msg "Unknown $t_opt -w option: $win_option" ;;
-        esac
-    else
-        glob_option="$($TMUX_BIN show-options -gv "$t_opt")"
-        case "$glob_option" in
-            single) lbl_single="-(glob) Single" ;;
-            double) lbl_double="-(glob) Double" ;;
-            heavy) lbl_heavy="-(glob) Heavy" ;;
-            simple) lbl_simple="-(glob) sImple" ;;
-            number) lbl_number="-(glob) Number" ;;
-            spaces) lbl_spaces="-(glob) Spaces" ;;
-            *) error_msg "Unknown $t_opt -g option: $glob_option" ;;
-        esac
-    fi
-
+    # TODO: option none below did not work as per man page as late as 26-09-02
+    #       disable is still broken by release
+    no_border="No border for floating panes"
     set -- \
+        3.2 T "-" \
+        3.2 T "-#[align=centre,nodim]pane-border-lines" \
         3.2 C s "$lbl_single" "$_cmd  single  $runshell_reload_mnu" \
         3.2 C d "$lbl_double" "$_cmd  double  $runshell_reload_mnu" \
-        3.2 C h "$lbl_heavy" "$_cmd   heavy   $runshell_reload_mnu" \
+        3.2 C h "$lbl_heavy" "$_cmd  heavy   $runshell_reload_mnu" \
         3.2 C i "$lbl_simple" "$_cmd  simple  $runshell_reload_mnu" \
-        3.2 C n "$lbl_number" "$_cmd  number  $runshell_reload_mnu" \
-        3.6 C p "$lbl_spaces" "$_cmd  spaces  $runshell_reload_mnu"
-    menu_generate_part 6 "$@"
+        3.2 C \\# "$lbl_number" "$_cmd  number  $runshell_reload_mnu" \
+        3.6 C p "$lbl_spaces" "$_cmd  spaces  $runshell_reload_mnu" \
+        3.7z C n "$no_border" "$_cmd  none    $runshell_reload_mnu"
+    menu_generate_part 5 "$@" #
 }
 
 dynamic_content() {
-    handle_pane_title
+    tmux_vers_check 2.3 && handle_pane_border_status
     tmux_vers_check 3.2 && handle_pane_border_lines
 }
 
@@ -97,15 +121,9 @@ static_content() {
     $cfg_display_cmds && display_commands_toggle 2
     set -- \
         0.0 S \
-        0.0 T "-#[align=centre,nodim]pane title"
+        2.6 C c "Change Title" "command-prompt -I '#T' -p 'Pane title: ' \
+            'select-pane -T \"%%\"' $runshell_reload_mnu"
     menu_generate_part 3 "$@"
-
-    set -- \
-        2.5 C c "Change" "command-prompt -I '#T' -p 'Pane title: ' \
-            'select-pane -T \"%%\"' $runshell_reload_mnu" \
-        3.2 T "-" \
-        3.2 T "-#[align=centre,nodim]pane border lines"
-    menu_generate_part 5 "$@"
 }
 
 #===============================================================
@@ -115,7 +133,7 @@ static_content() {
 #===============================================================
 
 menu_name="Layouts - Border Lines"
-menu_min_vers=2.5
+menu_min_vers=2.3
 
 #  Full path to tmux-menux plugin, remember to do one /.. for each subfolder
 D_TM_BASE_PATH=$(cd "${0%/*}/.." && pwd)
