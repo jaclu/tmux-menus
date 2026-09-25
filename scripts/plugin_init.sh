079ea019346b4a45aa41d724e0202c468bd46dbf
jaclu <jacob.lundqvist@gmail.com>
2026-09-25 13:05:30 +0200

    ensure TMUX_BIN is defined

--- scripts/plugin_init.sh
+++ scripts/plugin_init.sh
@@ -118,6 +118,7 @@ initialize_plugin=true
 f_skip_low_tmux_version_warning="$TMUX_MENUS_LOCATION"/.skip_old_tmux_warning
 
 # Set up plugin location in tmux env
+TMUX_BIN="${TMUX_BIN:-tmux}"
 TMUX_MENUS_LOCATION=$(cd "${0%/*}/.." && pwd)
 export TMUX_MENUS_LOCATION
 $TMUX_BIN set-environment -g TMUX_MENUS_LOCATION "$TMUX_MENUS_LOCATION"
